132a33502de376313a300a4177495a998471a105
Andre Gluderer <andre.gluderer@netcare.de>
2017-12-14 15:09:25 +0100

    made config a module export

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 /*
-var certs = {
+module.exports.certs = {
     key: fs.readFileSync('C:/TA_framework/emb-rt-ta/sslcert/emb-rt-ta.netcare.de-domain.key'),
    cert: fs.readFileSync('C:/TA_framework/emb-rt-ta/sslcert/emb-rt-ta.netcare.de_cert_single_11102017.crt')
 };
