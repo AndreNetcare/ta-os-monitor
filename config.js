aa4fc837285d6f325c7a04bfe5cd2af60a155fcb
Andre Gluderer <andre.gluderer@netcare.de>
2017-12-18 15:43:57 +0100

    added new cert files

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 module.exports.certs_retail = {
     /*
     key: fs.readFileSync('C:/TA_framework/emb-rt-ta/sslcert/emb-rt-ta.netcare.de-domain.key'),
-   cert: fs.readFileSync('C:/TA_framework/emb-rt-ta/sslcert/emb-rt-ta.netcare.de_cert_single_11102017.crt')
+   cert: fs.readFileSync('C:/TA_framework/emb-rt-ta/sslcert/emb-rt-ta.netcare.de_cert_single_18122017.crt')
    */
 };
 
 module.exports.certs_oneweb = {
     /*
     key: fs.readFileSync('C:/TA_framework/oneweb-ta/geotrust/oneweb-ta.netcare.de.key'),
-   cert: fs.readFileSync('C:/TA_framework/oneweb-ta/acme-cert/oneweb-ta.netcare.de_cert_single_11102017.crt')
+   cert: fs.readFileSync('C:/TA_framework/oneweb-ta/acme-cert/oneweb-ta_netcare.de_cert_18122017_single.crt')
    */
 };
