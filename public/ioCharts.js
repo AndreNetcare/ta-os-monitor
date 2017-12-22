ab651f581e7ed45a9b65b241db901086dc220958
Andre Gluderer <andre.gluderer@netcare.de>
2017-12-22 10:24:48 +0100

    added polar cahrt for cpu workload total, imporved memory bart chart

--- public/ioCharts.js
+++ public/ioCharts.js
@@ -1,21 +1,51 @@
 var memChart = null;
 var cpuChart = null;
 var memBarChart = null;
+var polarAreaChart = null;
 
 var cpuType = '';
 var noOfCpu = 0;
 
-var ctx = document.getElementById('memoryChart');
-var ctx2 = document.getElementById('cpuusageChart');
-var ctx3 = document.getElementById('memoryStackedBarChart');
+var doughnutChartCanvas = document.getElementById('memoryChart');
+var cpuChartCanvas = document.getElementById('cpuusageChart');
+var memoryStackedBarChartCanvas = document.getElementById('memoryStackedBarChart');
+var cpuPolarChartCanvas = document.getElementById('cpuusagePolar');
 
+//for polar cpu chart
+var cpuIdle = 0;
+var cpuTotalload = 0;
+var cpuJava = 0;
 
-
-
-
+var updateCount = 0;
 
 // And for a doughnut chart
 function ngOnInit() {
+
+  var cpuPolarData = {
+    labels: ["Idle","Load","Java"],
+    datasets: [{
+      data: [0, 0, 0],
+      backgroundColor: [
+        "rgba(255, 0, 0, 0.5)",
+        "rgba(100, 255, 0, 0.5)",
+        "rgba(200, 50, 255, 0.5)"
+      ]
+    }]
+  };
+
+this.polarAreaChart = new Chart(cpuPolarChartCanvas, {
+    type: 'polarArea',
+    data: cpuPolarData,
+    options: {
+      scale: {
+          ticks: {
+              beginAtZero: true,
+              max: 100
+          }
+      }
+    }
+  });
+
 
 var memBarChartData = {
   datasets: [{
@@ -30,7 +60,7 @@ var memBarChartData = {
   labels: [],
   };
 
-this.memBarChart = new Chart(ctx3, {
+this.memBarChart = new Chart(memoryStackedBarChartCanvas, {
     type: 'bar',
     data: memBarChartData,
     options: {
@@ -59,7 +89,7 @@ var doughnutGraphData = {
     ]
   };
 
-this.memChart  = new Chart(ctx, {
+this.memChart  = new Chart(doughnutChartCanvas, {
     type: 'doughnut',
     data: doughnutGraphData,
     maintainAspectRatio: true,
@@ -69,19 +99,19 @@ this.memChart  = new Chart(ctx, {
 var cpuLoadGraphData = {
     datasets: [{
       label: 'CPU Load System',
-      data: [],
+      data: [0,0,0,0,0],
       backgroundColor: 'rgba(75, 192, 192, 0.4)',
     },{ 
       label: "CPU Load Java",
-      data: [],
+      data: [0,0,0,0,0],
       borderColor: "#8e5ea2",
       fill: false
     }],
-    labels: ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    labels: ['x', 'x', 'x', 'x', 'x'],
 
   };
   
-  this.cpuChart = new Chart(ctx2, {
+  this.cpuChart = new Chart(cpuChartCanvas, {
     type: 'line',
     data: cpuLoadGraphData,
     maintainAspectRatio: true,
@@ -133,7 +163,7 @@ function updateCPUChart(event){
 
     
     cpuChart.data.datasets.forEach((dataset) => {
-        if ( dataset.data.length > 9) {
+        if ( dataset.data.length > 5) {
           dataset.data.shift();
         }
         dataset.data.push(event);
@@ -146,10 +176,16 @@ function updateCompCPUChart(event){
   
       
       
-          if (  cpuChart.data.datasets[0].data.length > 9) {
+          if (  cpuChart.data.datasets[0].data.length > 5) {
             cpuChart.data.datasets[0].data.shift();
+            cpuChart.data.labels.shift();
           }
           cpuChart.data.datasets[0].data.push(event);
+          updateCount++;
+          cpuTotalload += event;
+          cpuIdle += (100 - event);
+          var d = new Date();
+          cpuChart.data.labels.push('' + fomatTime(d.getHours()) + ':' + fomatTime(d.getMinutes()) + ':' + fomatTime(d.getSeconds()));
       
         
         cpuChart.update(0);
@@ -158,15 +194,26 @@ function updateCompCPUChart(event){
   function updateJavaCPUChart(event){
     
         
-    if (  cpuChart.data.datasets[1].data.length > 9) {
+    if (  cpuChart.data.datasets[1].data.length > 5) {
       cpuChart.data.datasets[1].data.shift();
     }
     cpuChart.data.datasets[1].data.push(event);
-
+    cpuJava += event;
   
   cpuChart.update(0);
+  updateCpuPolarChart();
     }
 
+
+    function updateCpuPolarChart(){
+      polarAreaChart.data.datasets[0].data.pop();
+      polarAreaChart.data.datasets[0].data.pop();
+      polarAreaChart.data.datasets[0].data.pop();
+      polarAreaChart.data.datasets[0].data.push(cpuIdle / updateCount);
+      polarAreaChart.data.datasets[0].data.push(cpuTotalload / updateCount);
+      polarAreaChart.data.datasets[0].data.push(cpuJava / updateCount);
+      polarAreaChart.update(0);
+    }
 
 function updateMemBarChart(event){
   
@@ -178,7 +225,7 @@ function updateMemBarChart(event){
         memBarChart.data.datasets[1].data.shift();
       }
       var d = new Date();
-      memBarChart.data.labels.push('' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+      memBarChart.data.labels.push('' + fomatTime(d.getHours()) + ':' + fomatTime(d.getMinutes()) + ':' + fomatTime(d.getSeconds()));
       //memBarChart.data.datasets[0].data.label.push(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
       memBarChart.data.datasets[0].label = ('Free: ' + formatBytes(event.freemem, 2));
       
@@ -202,6 +249,16 @@ function updateMemBarChart(event){
           sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
           i = Math.floor(Math.log(bytes) / Math.log(k));
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+      }
+
+      function fomatTime(time){
+        var formetedTime = '0';
+        if(time < 10){
+          formetedTime += time;
+          return formetedTime;
+        }else{
+          return time;
+        }
       }
 
 function connected(connectData) {
